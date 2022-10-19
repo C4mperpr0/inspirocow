9dc88355c17f0d8df4ee3a5c6f024b26dc8a5569
c4mperpr0 <carlbellgardt@gmail.com>
2022-10-19 09:20:29 +0200

    sync2

--- inspirocow.py
+++ inspirocow.py
@@ -1,11 +1,12 @@
 import requests
 from os import system as cmd
+from os import remove
 from io import BytesIO as Buffer
-from time import time
+from time import time, sleep
 
 r = requests.get("https://inspirobot.me/api?generate=true")
 img = requests.get(r.content.decode("utf-8"))
 filepath = f"/tmp/{round(time()*10000)}.jpg"
 with open(filepath, "wb+") as file:
     file.write(img.content)
-cmd(f"xcowsay -d {filepath} -t 7")
+cmd(f"xcowsay -d {filepath} -t 14 && rm {filepath}")
